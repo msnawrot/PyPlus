9735585a3620211836083046364a1a7e1c4f9884
Matt Narwot <msnawrot@gmail.com>
2020-10-16 10:24:30 -0400

    update w1ex2.py

--- w1ex2.py
+++ w1ex2.py
@@ -5,18 +5,17 @@ nxos1 = {
   'device_type': 'cisco_nxos',
   'host': 'nxos1.lasthop.io',
   'username': 'pyclass',
-  'password': '88newclass',
+  'password': getpass(),
 }
 
 nxos2 = {
   'device_type': 'cisco_nxos',
   'host': 'nxos2.lasthop.io',
   'username': 'pyclass',
-  'password': '88newclass',
+  'password': getpass(),
 }
 
 routers = [nxos1, nxos2]
 for i in routers:
     c = ConnectHandler(**i)
     print(c.find_prompt())
-    
