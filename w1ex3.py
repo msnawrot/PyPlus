75ff276cdb4357a39386a800ae781c7151bcdf2b
Matt Narwot <msnawrot@gmail.com>
2020-10-16 10:39:59 -0400

    modify w1ex3.py

--- w1ex3.py
+++ w1ex3.py
@@ -8,7 +8,14 @@ nxos1 = {
     'password': getpass('Password for nxos1: '),
 }
 
-routers = [nxos1]
+nxos2 = {
+  'device_type': 'cisco_nxos',
+  'host': 'nxos2.lasthop.io',
+  'username': 'pyclass',
+  'password': getpass(),
+}
+
+routers = [nxos1, nxos2]
 
 for router in routers:
     c = ConnectHandler(**router, session_log = './w1ex3.log.txt')
