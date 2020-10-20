70caa1ec8e0f9e39b2633df291f49ba968b346d1
Matt Narwot <msnawrot@gmail.com>
2020-10-20 19:00:47 -0400

    w2ex3.py

--- w2ex3.py
+++ w2ex3.py
@@ -15,4 +15,7 @@ output = net_connect.send_command('show version', use_textfsm=True)
 print(type(output))
 print(type(output[0]))
 print(output)
-pprint(net_connect.send_command('show lldp neighbors', use_textfsm=True))
+better_output = net_connect.send_command('show lldp neighbors', use_textfsm=True)
+pprint(better_output)
+# I need to print out the remote device's interface for the LLDP neighbor.
+print("The one LLDP neighbor is named %s and connects via it's interface %s." % (str(better_output['neighbor']), str(better_output['neighbor_interface'])))
