ac306810c39e9e3034bbeca595abc2eecc7f16c2
Matt Narwot <msnawrot@gmail.com>
2020-10-20 20:41:38 -0400

    new w2ex5.py and make_vlans.txt

---
+++ w2ex5.py
@@ -0,0 +1,29 @@
+from netmiko import *
+from getpass import getpass
+from datetime
+
+password = getpass("Type in the password:  ")
+
+# nxos1 (NX-OSv Switch)
+nxos1 = {
+    'host' : 'nxos1.lasthop.io',
+    'username' : 'pyclass',
+    'password' : password
+}
+# nxos2 (NX-OSv Switch)
+nxos2 = {
+    'host' : 'nxos2.lasthop.io',
+    'username' : 'pyclass',
+    'password' : password
+​​​​​​​}
+
+routers = [nxos1, nxos2]
+
+for router in routers:
+    net_connect = ConnectHandler(**router)
+    output = net_connect.send_config_from_file('make_vlans.txt')
+    if len(output) > 0:
+        print(f"successfully sent config to device, {router['host']}.")
+    print(output)
+    if net_connect.save_config():
+        print(f"successfully saved to startup config on {router['host']}.")
