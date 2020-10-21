626a9be44d50c2bedfe2de0bc293e2bb383d8656
Matt Narwot <msnawrot@gmail.com>
2020-10-21 18:07:19 -0400

    typo3

--- w2ex6.py
+++ w2ex6.py
@@ -1,6 +1,6 @@
 from netmiko import ConnectHandler, file_transfer
 from getpass import getpass
-import time 
+import time
 
 password = getpass()
 device = {
@@ -19,11 +19,11 @@ print(ssh_conn.config_mode())
 print(ssh_conn.find_prompt())
 print(ssh_conn.exit_config_mode())
 print(ssh_conn.find_prompt())
-print(ssh_conn.write_channel('disable\n'))
+ssh_conn.write_channel('disable\n')
 print("Waiting two seconds...")
 time.sleep(2.0)
 print(ssh_conn.read_channel())
 print(ssh_conn.enable()) #this method requires the secret key in the device dictionary
 print(ssh_conn.find_prompt())
-print(ssh_conn.disconnect())
+ssh_conn.disconnect()
 print("Remember to look at my_output.txt file!!")
