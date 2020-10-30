6521840163f120f0ca594bd286e4dc7bfd0a1d2c
Matt Narwot <msnawrot@gmail.com>
2020-10-29 21:45:58 -0400

    final w3ex7.py

--- w3ex7.py
+++ w3ex7.py
@@ -30,4 +30,17 @@ for nei in conf_nei:
     y = nei.children[0].text
     temp_tuple = (x, y)
     BGPpeers.append(temp_tuple)
+print("BGP Peers:")
 print(BGPpeers)
+
+# documenting Kirk's solution, because I think it is more elegant than mine
+# also, my script would hit on OSPF, EIGRP neighbors, not just BGP neighbors
+#neighbors = cisco_obj.find_objects_w_parents(
+#    parentspec=r"router bgp", childspec=r"neighbor"
+#)
+#for neighbor in neighbors:
+#    _, neighbor_ip = neighbor.text.split()
+#    for child in neighbor.children:
+#        if "remote-as" in child.text:
+#            _, remote_as = child.text.split()
+#    bgp_peers.append((neighbor_ip, remote_as))
