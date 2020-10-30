7a0fc7344d2949ef2faedc2b63ec765743d17a2e
Matt Narwot <msnawrot@gmail.com>
2020-10-29 20:15:06 -0400

    w3ex6.py

--- w3ex6.py
+++ w3ex6.py
@@ -1,8 +1,10 @@
 from netmiko import *
 import yaml
+from CiscoConfParse import CiscoConfParse
 
 with open("../.netmiko.yml") as f:
     yaml_out = yaml.load(f, Loader=yaml.FullLoader)
 
 net_connect = ConnectHandler(**yaml_out['cisco4'])
-print(net_connect.send_command("show run"))
+showrun = net_connect.send_command("show run")
+# showrun should be a list
