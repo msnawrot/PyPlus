959df39db06a91473c400cbf01e96d1357961df8
Matt Narwot <msnawrot@gmail.com>
2020-11-01 01:20:52 -0400

    w3ex1.py

--- w3ex1.py
+++ w3ex1.py
@@ -1,8 +1,20 @@
-myvar = [{'mac_addr' : '0062.ec29.70fe', 'ip_addr' : '10.220.88.1', 'interface': 'Gi0/0/0'}, {'mac_addr' : 'c89c.1dea.0eb6', 'ip_addr' : '10.220.88.20', 'interface': 'Gi0/0/0'}, {'mac_addr' : 'a093.5141.b780', 'ip_addr' : '10.220.88.22', 'interface': 'Gi0/0/0'}, {'mac_addr' : '0001.00ff.0001', 'ip_addr' : '10.220.88.37', 'interface': '/Gi0/0/0/'}, {'mac_addr' : '0002.00ff.0001', 'ip_addr' : '10.220.88.38', 'interface': 'Gi0/0/0'}]
+from pprint import pprint
 
-for d in myvar:
-    print(d['mac_addr'])
-    print()
-    print(d['ip_addr'])
-    print()
-    print(d['interface'])
+
+arp_data = """
+Protocol  Address          Age (min)  Hardware Addr   Type   Interface
+Internet  10.220.88.1            67   0062.ec29.70fe  ARPA   GigabitEthernet0/0/0
+Internet  10.220.88.20           29   c89c.1dea.0eb6  ARPA   GigabitEthernet0/0/0
+Internet  10.220.88.22            -   a093.5141.b780  ARPA   GigabitEthernet0/0/0
+Internet  10.220.88.37          104   0001.00ff.0001  ARPA   GigabitEthernet0/0/0
+Internet  10.220.88.38          161   0002.00ff.0001  ARPA   GigabitEthernet0/0/0
+"""
+
+arp_list = arp_data.splitlines()
+arp_list = arp_list[1:]
+new_structure = []
+for line in arp_list:
+    _, ip_addr, _, hw_addr, _, intfc = line.split()
+    new_structure.append({"ip address": ip_addr, "hw address": hw_addr, "interface": intfc})
+
+pprint(new_structure)
