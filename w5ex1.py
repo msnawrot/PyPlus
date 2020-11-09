26b2492cf5a526721686d40ba06c154f49b02a70
Matt Narwot <msnawrot@gmail.com>
2020-11-08 20:27:56 -0500

    w5ex1.py

--- w5ex1.py
+++ w5ex1.py
@@ -1,20 +1,32 @@
 from jinja2 import Template
 
+# bgp_config = """
+# router bgp {{ local_as }}
+#   neighbor {{ peer1_ip }} remote-as {{ peer1_as }}
+#     update-source loopback99
+#     ebgp-multihop 2
+#     address-family ipv4 unicast
+#   neighbor {{ peer2_ip }} remote-as {{ peer2_as }}
+#     address-family ipv4 unicast
+# """
 bgp_config = """
 router bgp {{ local_as }}
-  neighbor {{ peer1_ip }} remote-as {{ peer1_as }}
+  {% for neighbor in neighbors %}
+  neighbor {{ neighbor["peer_ip"] }} remote-as {{ neighbor["peer_as"] }}
+    address-family ipv4 unicast
+    {% if peer_ip == "10.1.20.2" %}
     update-source loopback99
     ebgp-multihop 2
-    address-family ipv4 unicast
-  neighbor {{ peer2_ip }} remote-as {{ peer2_as }}
-    address-family ipv4 unicast
+    {% endif %}
+  {% endfor %}
 """
 my_dict = {
     "local_as": 10,
-    "peer1_ip": "10.1.20.2",
-    "peer1_as": 20,
-    "peer2_ip": "10.1.30.2",
-    "peer2_as": 30
+    "neighbors": [{
+        "peer_ip": "10.1.20.2",
+        "peer_as": 20}, {
+        "peer_ip": "10.1.30.2",
+        "peer_as": 30}]
 }
 j2_template = Template(bgp_config)
 # output = j2_template.render(local_as=10, peer1_ip="10.1.20.2", peer1_as=20, peer2_ip="10.1.30.2", peer2_as=30)
