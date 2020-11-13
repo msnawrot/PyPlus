e1b8f10e2d35a89bffda1713f1d9f4ab70293dbd
Matt Narwot <msnawrot@gmail.com>
2020-11-13 18:23:27 -0500

    w5ex2a

--- w5ex2a.py
+++ w5ex2a.py
@@ -4,19 +4,12 @@ from jinja2.environment import Environment
 env = Environment(undefined=StrictUndefined)
 env.loader = FileSystemLoader([".", "./jinja2"])
 
-my_dict = {
-    "hostlist": [ {
-        "hostname": "nxos1",
-        "interface": "Ethernet1/1",
-        "ip_addr": "10.1.100.1",
-        "netmask": "24" }, {
-        "hostname": "nxos2",
-        "interface": "Ethernet1/1",
-        "ip_addr": "10.1.100.2",
-        "netmask": "24"} ]
-}
+nxos1 = {"interface": "Ethernet1/1", "ip_addr": "10.1.100.1", "netmask": "24"}
+nxos2 = {"interface": "Ethernet1/1", "ip_addr": "10.1.100.2", "netmask": "24"}
 
 j2_template = "w5ex2a_tpl.j2"
-template = env.get_template(j2_template)
-output = template.render(**my_dict)
-print(output)
+
+for j2_vars in (nxos1, nxos2):
+    template = env.get_template(j2_template)
+    output = template.render(**j2_vars)
+    print(output)
