2e7fdbd6836ba8e7728333589721e2dc320bc34e
Matt Narwot <msnawrot@gmail.com>
2020-12-09 13:19:11 -0500

    w6 ex1 done

--- w6ex1.py
+++ w6ex1.py
@@ -13,7 +13,6 @@ show_cmd = "show ip arp"
 
 device = pyeapi.client.Node(connection)
 output = device.enable(show_cmd)
-print(output)
 #  From this ARP table data, print out a mapping of all of the IP addresses and
 # their corresponding MAC addresses.
 
@@ -21,7 +20,6 @@ print(output)
 # convert output to a list of tuples
 ip_mac_list = list()
 for addr_dict in output[0]['result']['ipV4Neighbors']:
-    print(addr_dict)
     ip_addr = addr_dict['address']
     mac_addr = addr_dict['hwAddress']
     ip_mac_list.append((ip_addr, mac_addr))
