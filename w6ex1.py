10d46c19999ce7547a5d3274e589ee549244d350
Matt Narwot <msnawrot@gmail.com>
2020-12-09 13:18:35 -0500

    w6 ex1 done

--- w6ex1.py
+++ w6ex1.py
@@ -16,3 +16,17 @@ output = device.enable(show_cmd)
 print(output)
 #  From this ARP table data, print out a mapping of all of the IP addresses and
 # their corresponding MAC addresses.
+
+
+# convert output to a list of tuples
+ip_mac_list = list()
+for addr_dict in output[0]['result']['ipV4Neighbors']:
+    print(addr_dict)
+    ip_addr = addr_dict['address']
+    mac_addr = addr_dict['hwAddress']
+    ip_mac_list.append((ip_addr, mac_addr))
+
+# print the list of tuples with a for loop
+print("IP Address\tMAC Address")
+for ip_addr, mac_addr in ip_mac_list:
+    print(ip_addr, "\t", mac_addr)
