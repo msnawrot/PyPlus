a53bc07a3130e23b83fbed0df8ec851c450ecd56
Matt Narwot <msnawrot@gmail.com>
2020-12-09 18:12:05 -0500

    w6ex2b.py

--- my_funcs.py
+++ my_funcs.py
@@ -11,9 +11,16 @@ def run_show_command(cmd, **device):
     connection = pyeapi.client.connect(**device)
     device = pyeapi.client.Node(connection)
     output = device.enable(cmd)
+    return output
+
+if __name__ == "__main__":
+    device_dict = read_yaml('w6ex2a.yml')
+    device_dict['password'] = getpass("password please: ")
+    show_cmd = "show ip arp"
+    result = run_show_command(show_cmd, **device_dict)
     # convert output to a list of tuples
     ip_mac_list = list()
-    for addr_dict in output[0]['result']['ipV4Neighbors']:
+    for addr_dict in result[0]['result']['ipV4Neighbors']:
         ip_addr = addr_dict['address']
         mac_addr = addr_dict['hwAddress']
         ip_mac_list.append((ip_addr, mac_addr))
@@ -21,9 +28,3 @@ def run_show_command(cmd, **device):
     print("IP Address\t MAC Address")
     for ip_addr, mac_addr in ip_mac_list:
         print(ip_addr, "\t", mac_addr)
-
-if __name__ == "__main__":
-    device_dict = read_yaml('w6ex2a.yml')
-    device_dict['password'] = getpass("password please: ")
-    show_cmd = "show ip arp"
-    run_show_command(show_cmd, **device_dict)
