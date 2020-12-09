d3151e8953b65f5a2879e3b60655e10c87aaa8be
Matt Narwot <msnawrot@gmail.com>
2020-12-09 18:33:18 -0500

    w6ex2b.py

--- w6ex3.py
+++ w6ex3.py
@@ -6,3 +6,12 @@ device_dict = read_yaml('w6ex2a.yml')
 device_dict['password'] = getpass("password please: ")
 show_cmd = "show ip route"
 result = run_show_command(show_cmd,**device_dict)
+# LOL look how long this is!!!
+# print(result[0]['result']['vrfs']['default']['routes']['10.220.88.0/24']['vias'][0]['interface'])
+# Vlan1
+
+for k, v in result[0]['result']['vrfs']['default']['routes'].items():
+    print("\n" + k, "\t", v['routeType'], end = '')
+    if v['routeType'] == "static":
+        for i in v['vias']:
+            print("\t" +i['nexthopAddr'])
