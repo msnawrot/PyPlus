1784eaebfc113f5dadadad5ebfa67ef1575706c3
Matt Narwot <msnawrot@gmail.com>
2020-12-09 18:08:53 -0500

    w6ex2b.py

--- w6ex3.py
+++ w6ex3.py
@@ -1,6 +1,6 @@
 import my_funcs
 
-device_dict = my_func.read_yaml('w6ex2a.yml')
+device_dict = my_funcs.read_yaml('w6ex2a.yml')
 device_dict['password'] = getpass("password please: ")
 show_cmd = "show ip route"
 my_funcs.run_show_command(show_cmd,**device_dict)
