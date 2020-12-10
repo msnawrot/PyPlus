1b608ecd4f0b8a4c91935f01848b778362278990
Matt Narwot <msnawrot@gmail.com>
2020-12-09 21:05:33 -0500

    w4ex4.py

---
+++ w6ex4b.py
@@ -0,0 +1,27 @@
+import yaml
+import pyeapi
+from getpass import getpass
+from jinja2 import Template
+
+template = "no interface {{ intf_name }}"
+
+def read_yaml(filename):
+    with open(filename) as f:
+        yaml_out = yaml.load(f, Loader=yaml.FullLoader)
+    return yaml_out
+
+
+devices = read_yaml('w6ex4.yml')
+# loop interates over each switch
+# within the loop, first a jinja2 template is rendered,
+# second, the config is pushed to the switch
+# foobar needs to be the data dictionary
+for switch in devices:
+    for key, eapi_stuff in switch.items():
+        j2_template = Template(template)
+        cfg = j2_template.render(**eapi_stuff['data'])
+        cfg = cfg.splitlines()
+        eapi_stuff['password'] = getpass()
+        connection = pyeapi.client.connect(**eapi_stuff)
+        device = pyeapi.client.Node(connection)
+        output = device.config(cfg)
