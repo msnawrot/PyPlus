32c3e7ce2e0172d42b1ded3d50d1842b01b08a88
Matt Narwot <msnawrot@gmail.com>
2020-12-09 21:10:06 -0500

    w4ex4.py

--- w6ex4.py
+++ w6ex4.py
@@ -13,7 +13,7 @@ def read_yaml(filename):
 
 
 devices = read_yaml('w6ex4.yml')
-pass = getpass("Password :")
+passwd = getpass("Password :")
 # loop interates over each switch
 # within the loop, first a jinja2 template is rendered,
 # second, the config is pushed to the switch
@@ -23,7 +23,7 @@ for switch in devices:
         j2_template = Template(template)
         cfg = j2_template.render(**eapi_stuff['data'])
         cfg = cfg.splitlines()
-        connection = pyeapi.client.connect(password=pass,**eapi_stuff)
+        connection = pyeapi.client.connect(password=passwd,**eapi_stuff)
         device = pyeapi.client.Node(connection)
         output = device.config(cfg)
 
