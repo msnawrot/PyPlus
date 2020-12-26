6575858af0415d0f8bf147e2c723e82695a7c237
Matt Narwot <msnawrot@gmail.com>
2020-12-26 14:12:50 -0500

    w7ex6

--- w7ex6.py
+++ w7ex6.py
@@ -20,4 +20,9 @@ device = Device(
 raw_output = device.show("show interface Ethernet1/1")
 # parse the output
 print(raw_output)
+int_name = raw_output['TABLE_interface']['ROW_interface']['interface']
+int_state = raw_output['TABLE_interface']['ROW_interface']['state']
+int_mtu = raw_output['TABLE_interface']['ROW_interface']['eth_mtu']
+
 # print out the following information:
+print("Interface:", int_name + "; State:", int_state, "MUT:", int_mtu)
