c9eee9a5c39da846f30e5fa4e78b5d8ac5255e01
Matt Narwot <msnawrot@gmail.com>
2021-01-04 16:40:33 -0500

    w8ex2

---
+++ jnpr_devices.py
@@ -0,0 +1,7 @@
+from getpass import getpass
+
+srx2 = {
+    'host': "srx2.lasthop.io",
+    'password': getpass(),
+    'user': "pyclass",
+}
---
+++ w8ex2b.py
@@ -0,0 +1,28 @@
+import jnpr_devices
+from jnpr.junos import Device
+
+def check_connected():
+    print()
+
+def gather_routes():
+    print()
+
+def gather_arp_table():
+    print()
+
+def print_output():
+    print()
+
+if __name__ == "__main__":
+    # make a Device
+
+    # connect to device
+
+    # check_connected
+
+    # gather_routes
+
+    # gather_arp_table
+
+    # print_output
+    
