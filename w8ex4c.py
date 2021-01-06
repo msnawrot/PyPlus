a01e131e3de37a196d336ab1319509d14a5d15a8
Matt Narwot <msnawrot@gmail.com>
2021-01-05 22:18:33 -0500

    w8ex4c.py

--- w8ex4c.py
+++ w8ex4c.py
@@ -3,6 +3,14 @@ from w8ex2b import check_connected, gather_routes
 from jnpr.junos.utils.config import Config
 from jnpr.junos import Device
 
+def non_match_elements(list_a, list_b):
+    non_match = []
+    for i in list_a:
+        if i not in list_b:
+            non_match.append(i)
+    return non_match
+
+
 # make a Device
 my_device = Device(**srx2)
 # connect to device
@@ -22,3 +30,5 @@ if check_connected(my_device) == True:
     cfg.commit()
     cfg.unlock()
     post_routes = gather_routes(my_device)
+    non_match = non_match_elements(pre_routes, post_routes)
+    print("New routes: ", non_match)
