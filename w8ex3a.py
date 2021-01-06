800dfb8492181b341115c8daf9a667a9427f7129
Matt Narwot <msnawrot@gmail.com>
2021-01-05 20:59:37 -0500

    w8ex3a.py

---
+++ w8ex3a.py
@@ -0,0 +1,20 @@
+from jnpr_devices import srx2
+from jnpr.junos import Device
+from jnpr.junos.op.routes import RouteTable
+from jnpr.junos.op.arp import ArpTable
+import w8ex2b.py
+from jnpr.junos.utils.config import Config
+
+if __name__ == "__main__":
+    # make a Device
+    my_device = Device(**srx2)
+    # connect to device
+    my_device.open()
+    # check_connected
+    if check_connected(my_device) == True:
+        cfg = Config(my_device)
+        cfg.lock()
+    try:
+        cfg.lock()
+    except LockError as e:
+        raise e
