524e3eff1f92d8c27af90161bb8a72a31ec09c72
Matt Narwot <msnawrot@gmail.com>
2021-01-05 23:02:32 -0500

    w8ex5a.py

--- w8ex5b.py
+++ w8ex5b.py
@@ -5,5 +5,5 @@ from pprint import pprint
 
 my_device = Device(**srx2)
 my_device.open()
-xml_out = my_device.rpc.get_interface_information()
+xml_out = my_device.rpc.terse()
 print(etree.tostring(xml_out, encoding="unicode"))
