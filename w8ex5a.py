9cee3fc9d003be69d24150bdf06957718a324787
Matt Narwot <msnawrot@gmail.com>
2021-01-05 22:54:25 -0500

    w8ex5a.py

--- w8ex5a.py
+++ w8ex5a.py
@@ -3,7 +3,7 @@ from lxml import etree
 from w8ex2b import srx2
 from pprint import pprint
 
-my_device = Device(srx2)
+my_device = Device(**srx2)
 my_device.open()
 xml_out = my_device.rpc.get_software_information()
 pprint(etree.tostring(xml_out, encoding="unicode", pretty_print=True))
