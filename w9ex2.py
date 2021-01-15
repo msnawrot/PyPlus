92f78c1b190a1a2be83a76621408b02d7b1482d8
Matt Narwot <msnawrot@gmail.com>
2021-01-14 21:40:18 -0500

    w9ex2

--- w9ex2.py
+++ w9ex2.py
@@ -1,5 +1,5 @@
 from my_devices import network_devices
-from my_functions import open_napalm_connection
+from my_functions import open_napalm_connection, create_backup
 from pprint import pprint
 
 connections_list = []
@@ -13,16 +13,16 @@ for conn in connections_list:
     arp_table = conn.get_arp_table()
     print(conn.hostname, ":")
     pprint(arp_table)
-    print("-" * 20)
+    print("-" * 40)
     print()
 
 # 2c attempt to use get_ntp_peers() method against both devices. gracefully handle exceptions
 for conn in connections_list:
     try:
         ntp_peers = conn.get_ntp_peers()
-        print(conn.hostname, ";")
+        print(conn.hostname, ":")
         pprint(ntp_peers)
-        print("-" * 20)
+        print("-" * 40)
         print()
     except Exception as ex:
         print(ex)
