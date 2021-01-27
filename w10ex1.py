912b7f27b2b8cce03c89f8fb3737f0c50272e0d3
Matt Narwot <msnawrot@gmail.com>
2021-01-27 17:10:30 -0500

    w10 notes & ex1

--- w10ex1.py
+++ w10ex1.py
@@ -2,7 +2,7 @@ from Netmiko import ConnectHandler
 from w10my_devices import network_devices as devices
 from datetime import datetime
 
-def show_command(**device, command):
+def show_command(command, **device):
     connection = ConnectHandler(**device)
     output = connection.send_command(command)
     connection.disconnect()
@@ -14,7 +14,7 @@ start_time = datetime.now()
 for device in devices:
     print("=" * 40)
     print("Device: " + device['hostname'])
-    print(show_command(device, my_command))
+    print(show_command(my_command, device))
     print("+" * 40)
 
 print("Elapsed time: " + datetime.now() - str(start_time))
