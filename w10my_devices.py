02fa8f78822811547d66230043db2379b10db277
Matt Narwot <msnawrot@gmail.com>
2021-01-27 17:21:09 -0500

    w10 notes & ex1

--- w10my_devices.py
+++ w10my_devices.py
@@ -2,14 +2,14 @@ from getpass import getpass
 my_password = getpass()
 
 cisco3 = {
-    'hostname':"cisco3.lasthop.io",
+    'host':"cisco3.lasthop.io",
     'device_type':"cisco_ios",
     'username':"pyclass",
     'password': my_password,
 }
 
 arista1 = {
-    'hostname':"arista1.lasthop.io",
+    'host':"arista1.lasthop.io",
     'device_type':"arista_eos",
     'username':"pyclass",
     'password': my_password,
@@ -17,7 +17,7 @@ arista1 = {
 }
 
 arista2 = {
-    'hostname':"arista2.lasthop.io",
+    'host':"arista2.lasthop.io",
     'device_type':"arista_eos",
     'username':"pyclass",
     'password': my_password,
@@ -25,7 +25,7 @@ arista2 = {
 }
 
 srx2 = {
-    'hostname':"srx2.lasthop.io",
+    'host':"srx2.lasthop.io",
     'device_type':"juniper_junos",
     'username':"pyclass",
     'password': my_password,
