af2ea848c05754c38040d2ae697c8d7ec884108a
Matt Narwot <msnawrot@gmail.com>
2021-01-27 18:00:32 -0500

    w10 ex2

--- w10my_functions.py
+++ w10my_functions.py
@@ -2,7 +2,7 @@ from netmiko import ConnectHandler
 
 def ssh_command(command, **device):
     print("=" * 40)
-    print("Device: " + device['host'])
+    print("Device: " + device)
     connection = ConnectHandler(**device)
     output = connection.send_command(command)
     print(output)
