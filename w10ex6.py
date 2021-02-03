b61083543a4b24f13adfa1faf01aa601ea084170
Matt Narwot <msnawrot@gmail.com>
2021-02-02 22:16:18 -0500

    w10ex6

--- w10ex6.py
+++ w10ex6.py
@@ -1,7 +1,13 @@
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from w10my_devices import network_devices as devices
 from datetime import datetime
-from w10my_functions import ssh_command2
+
+# defining ssh_command2 here b/c the version in w10my_functinos has opposite order of arguments
+def ssh_command2(device, command):
+    connection = ConnectHandler(**device)
+    output = connection.send_command(command)
+    connection.disconnect()
+    return output
 
 my_command = "show version"
 start_time = datetime.now()
@@ -9,7 +15,7 @@ start_time = datetime.now()
 max_threads = 4
 
 with ProcessPoolExecutor(max_threads) as pool:
-    results_generator = pool.map(ssh_command2, my_command, devices)
+    results_generator = pool.map(ssh_command2, devices, my_command)
     for result in results_generator:
         print("Result: " + result)
 
