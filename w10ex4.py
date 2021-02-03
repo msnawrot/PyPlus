09656f737deb0303dfa86717c30807f89b5c71bf
Matt Narwot <msnawrot@gmail.com>
2021-02-02 22:02:45 -0500

    w10ex4

--- w10ex4.py
+++ w10ex4.py
@@ -1,4 +1,4 @@
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from w10my_devices import network_devices as devices
 from datetime import datetime
 from w10my_functions import ssh_command2
@@ -7,10 +7,15 @@ my_command = "show version"
 start_time = datetime.now()
 
 max_threads = 4
+pool = ProcessPoolExecutor(max_threads)
 
-with ProcessPoolExecutor(max_threads) as pool:
-    results_generator = pool.map(ssh_command2, my_command, devices)
-    for result in results_generator:
-        print("Result: " + result)
+futures = []
+for device in devices:
+    future = pool.submit(ssh_command2, my_command, device)
+    futures.append(future)
+
+
+for future in as_completed(futures):
+    print("Result: " + future.result())
 
 print("\nElapsed time: " + str(datetime.now() - start_time))
