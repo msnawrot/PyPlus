3d06167e1551cd752d7f353b6ebb31389decb120
Matt Narwot <msnawrot@gmail.com>
2021-02-05 16:58:51 -0500

    bonusRESTAI ex2

--- bonusRESTAPIex2.py
+++ bonusRESTAPIex2.py
@@ -1,14 +1,14 @@
+import os
 import requests
 from urllib3.exceptions import InsecureRequestWarning
+from pprint import pprint
 
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 if __name__ == "__main__":
     url = "https://netbox.lasthop.io/api/"
-    http_headers = {"accept": "application/json; version=2.4;"}
-    # if token:
-    #     http_headers["authorization"] = "Token {}".format(token)
-
+    http_headers = {}
+    http_headers['accept'] = "application/json; version=2.4;"
     response = requests.get(url, headers=http_headers, verify=False)
 
     # print the HTTP status code, the reesponse text, the json response, and the
@@ -23,3 +23,10 @@ if __name__ == "__main__":
     print(response.json())
     print("\nresponse HTTP headers:\n")
     print(response.headers)
+
+    print("*" * 10)
+    print("\nRetreiving all endpoints under /api/dcim parent\n")
+    url = "https://netbox.lasthop.io/api/dcim/devices/"
+    http_headers['Authorization'] = os.environ["NETBOX_TOKEN"]
+    response = requests.get(url, headers=http_headers, verify=False)
+    pprint(response.json())
