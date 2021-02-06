990b81d3bcd1574c1f7db60c7c4093392a5feaa1
Matt Narwot <msnawrot@gmail.com>
2021-02-06 12:07:31 -0500

    bonusRESTAPI ex5

---
+++ bonusRESTAPIex5.py
@@ -0,0 +1,26 @@
+import os
+import requests
+from urllib3.exceptions import InsecureRequestWarning
+from pprint import pprint
+import json
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+if __name__ == "__main__":
+    print("*" * 10)
+    http_headers = {}
+    http_headers['Content-Type'] = "application/json; version=2.4;"
+    http_headers['accept'] = "application/json; version=2.4;"
+    token = os.environ["NETBOX_TOKEN"]
+    http_headers['Authorization'] = f"Token {token}"
+    url = "https://netbox.lasthop.io/api/ipam/ip-addresses/286/"
+    put_data = {
+        "address": "192.0.2.101/32",
+        "description": ""
+        }
+
+    response = requests.put(url, headers=http_headers, data=json.dumps(put_data), verify=False)
+    print("RESPONSE CODE:", response.status_code)
+    print("RESPONSE DATA:", response.json())
+
+# {"id":277,"family":4,"address":"192.0.2.100/32","vrf":null,"tenant":null,"status":{"value":1,"label":"Active"},"role":null,"interface":null,"description":"","nat_inside":null,"nat_outside":null,"tags":[],"custom_fields":{},"created":"2021-01-27","last_updated":"2021-01-27T16:46:37.742458-08:00"}
