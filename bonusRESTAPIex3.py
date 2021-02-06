50851417b5d4720e5c067d91114ed4249aa0a66f
Matt Narwot <msnawrot@gmail.com>
2021-02-05 19:42:49 -0500

    bonusRESTAPI ex3

--- bonusRESTAPIex3.py
+++ bonusRESTAPIex3.py
@@ -8,13 +8,14 @@ requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 if __name__ == "__main__":
     http_headers = {}
     http_headers['accept'] = "application/json; version=2.4;"
-
     print("*" * 10)
     print("\nRetreiving all devices.\n")
     url = "https://netbox.lasthop.io/api/dcim/devices/"
     token = os.environ["NETBOX_TOKEN"]
     http_headers['Authorization'] = f"Token {token}"
     response = requests.get(url, headers=http_headers, verify=False)
+    tempresults = response.json()
+    results = tempresults['results']
 
-    for device in response['results']:
+    for device in results:
         print(device['display_name'])
