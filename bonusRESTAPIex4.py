d9ddcc25c76ab4c6fed7ab34a33e35d0c4f2d605
Matt Narwot <msnawrot@gmail.com>
2021-02-06 11:48:20 -0500

    bonusRESTAPI ex4

--- bonusRESTAPIex4.py
+++ bonusRESTAPIex4.py
@@ -7,15 +7,16 @@ requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 if __name__ == "__main__":
     print("*" * 10)
-    print("\nRetreiving all devices.\n")
     http_headers = {}
     http_headers['Content-Type'] = "application/json; version=2.4;"
     http_headers['accept'] = "application/json; version=2.4;"
     token = os.environ["NETBOX_TOKEN"]
     http_headers['Authorization'] = f"Token {token}"
     url = "https://netbox.lasthop.io/api/ipam/ip-addresses/"
-    post_data = { "address": "192.0.2.100/32"}
+    post_data = { "address": "192.0.2.101/32"}
 
     response = requests.post(url, headers=http_headers, data=post_data, verify=False)
     print("RESPONSE CODE:", response.status_code)
-    print("RESPONSE DATA:", response.json())
+    print("RESPONSE DATA:", response.text)
+
+# {"id":277,"family":4,"address":"192.0.2.100/32","vrf":null,"tenant":null,"status":{"value":1,"label":"Active"},"role":null,"interface":null,"description":"","nat_inside":null,"nat_outside":null,"tags":[],"custom_fields":{},"created":"2021-01-27","last_updated":"2021-01-27T16:46:37.742458-08:00"}
